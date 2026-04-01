15846014336fca3e95f927bc70ad19d200f1799a
strize <hi@strize.dev>
2026-04-01 12:23:35 +0530

    more wallpapers

--- modules/DEs/niri.nix
+++ modules/DEs/niri.nix
@@ -3,7 +3,6 @@
 {
   programs.niri.enable = true;
   programs.dms-shell.enable = true;
-  programs.dsearch.enable = true;
   environment.systemPackages = with pkgs; [
     xwayland-satellite
     quickshell
